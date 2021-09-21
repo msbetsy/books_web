f332145f019e6ab7d9bc8924dfb2a254fbba6f78
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-09-21 16:16:49 +0200

    Edit admin view -> add filter, searching fields, change list display

--- books/book_shelf/admin.py
+++ books/book_shelf/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Book
 
+
 # Register your models here.
-admin.site.register(Book)
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'language', 'status')
+    list_filter = ('genre', 'status', 'year', 'language')
+    search_fields = ('title', 'author')
+    ordering = ('title', 'year')
