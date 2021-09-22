4b504a88149ec9a9bec048c23962d9c3f8398442
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-09-22 15:42:58 +0200

    Add get_absolute_url method to Book model

--- books/book_shelf/models.py
+++ books/book_shelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -45,3 +46,8 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """Create canonical URL."""
+
+        return reverse('shelf:book_info', args=[self.year, self.id])
